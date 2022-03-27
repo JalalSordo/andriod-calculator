9755f63f025c9420bb4a26910d3a11ff2d5ef56a
Jalal Sordo <jalal_sordo@hotmail.com>
2022-03-27 14:55:19 +0000

    working app, layout problems

--- app/src/main/java/stic/cdam/confapp/MainActivity.java
+++ app/src/main/java/stic/cdam/confapp/MainActivity.java
@@ -1,17 +1,13 @@
 package stic.cdam.confapp;
-
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.ui.AppBarConfiguration;
 
 import stic.cdam.confapp.databinding.ActivityMainBinding;
 
+
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
     @Override
@@ -22,39 +18,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
 
-        //appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        //  NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        // if (id == R.id.action_settings) {
-        //     return true;
-        // }
-
-        return super.onOptionsItemSelected(item);
-    }
-/*
-    @Override
-    public boolean onSupportNavigateUp() {
-        //NavController navController = Navigation.findNavController(this, R.id.action_FirstFragment_to_SecondFragment);
-       // return NavigationUI.navigateUp(navController, appBarConfiguration)
-            //    || super.onSupportNavigateUp();
-
-    }*/
 }
