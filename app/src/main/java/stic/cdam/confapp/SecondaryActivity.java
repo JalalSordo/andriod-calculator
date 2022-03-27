9924b80f76994c1cea45991b89fefcb69cb65dd4
Jalal Sordo <jalal_sordo@hotmail.com>
2022-03-27 04:13:29 +0000

    Initial commit

---
+++ app/src/main/java/stic/cdam/confapp/SecondaryActivity.java
@@ -0,0 +1,19 @@
+package stic.cdam.confapp;
+
+
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondaryActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+}
